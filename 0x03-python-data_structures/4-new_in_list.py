e5519ca60787db8ad4004d4370b0edeaee9c2a9c
DIGeagh <darlingtongavi@gmail.com>
2023-06-12 09:02:17 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 
 
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position without modifying the original list."""
+    """ Replace an element in a copied list at a specific position."""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
