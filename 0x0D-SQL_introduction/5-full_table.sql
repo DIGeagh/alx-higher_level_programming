2b45819ce44549d0950dd37a937c2fa49893cf35
DIGeagh <darlingtongavi@gmail.com>
2023-08-15 19:49:28 -0700

    SQL

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- Prints the full description of the table first_table
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
 WHERE TABLE_SCHEMA = 'database_name'
 AND TABLE_NAME = 'first_table';
