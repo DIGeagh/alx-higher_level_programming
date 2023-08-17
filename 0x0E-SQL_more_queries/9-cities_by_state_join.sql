fd2ff3b81b5dd37cb05cec08ef255762ecad1c26
DIGeagh <darlingtongavi@gmail.com>
2023-08-16 21:17:21 -0700

    SQL2

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,8 +0,0 @@
--- Use the specified database
-USE <database_name>;
-
--- List cities with required information and sorting
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
