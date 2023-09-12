73c4f3aebfb35ad03c359547679fa05073749443
DIGeagh <darlingtongavi@gmail.com>
2023-09-12 11:52:26 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
+
 class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      // Initialize instance attributes width and height
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is <= 0
+      return {};
+    }
   }
 }
 
