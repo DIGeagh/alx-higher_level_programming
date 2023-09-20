f7dfd9fdd37721eaf4832a8cf10f4eec7f752f99
DIGeagh <darlingtongavi@gmail.com>
2023-09-20 15:17:16 -0700

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Import the add function from the add.js file
+const add = require('./add');
+
+// Example usage
+const result = add(5, 3);
+console.log(result); // This should print 8
