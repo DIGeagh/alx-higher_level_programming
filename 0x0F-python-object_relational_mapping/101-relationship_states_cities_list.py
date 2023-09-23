7ebdf5750ca2fad1c0eee2fa1ab0e1a715344f48
DIGeagh <darlingtongavi@gmail.com>
2023-09-23 08:09:45 -0700

    Mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Module that lists all State objects, and corresponding City objects,\
-        contained in the mySQL database"""
+"""
+Module that lists all State objects, and corresponding City objects,
+contained in the mySQL database
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
@@ -13,6 +15,9 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    # Create all tables in the database (including State and City tables)
+    Base.metadata.create_all(engine)
+
     # Create a session factory bound to the engine
     Session = sessionmaker(bind=engine)
 
@@ -20,11 +25,13 @@ if __name__ == "__main__":
     session = Session()
 
     # Retrieve all states from the database and order them by ID
-    for state in session.query(State).order_by(State.id):
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
         # Print state ID and name
         print("{}: {}".format(state.id, state.name))
 
         # Iterate over the cities associated with the current state
         for city in state.cities:
-            # Print city ID and name
+            # Print city ID and name with proper indentation
             print("    {}: {}".format(city.id, city.name))
