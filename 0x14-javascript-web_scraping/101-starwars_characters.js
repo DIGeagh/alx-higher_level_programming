b4768b88320ae53b31463dc2c78e9738bba74740
DIGeagh <darlingtongavi@gmail.com>
2023-10-24 14:53:35 -0700

    web scrapping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,41 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-    const characters = [];
-
-    // Create an array of promises for character data requests
-    const characterPromises = characterUrls.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-          if (charError) {
-            reject(charError);
-          } else {
-            const characterData = JSON.parse(charBody);
-            characters.push(characterData.name);
-            resolve();
-          }
-        });
-      });
-    });
-
-    // Resolve all promises and print characters in order
-    Promise.all(characterPromises)
-      .then(() => {
-        characters.forEach((characterName) => {
-          console.log(characterName);
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,20 +2,13 @@
 
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Response from ${url} saved to ${filePath}`);
-      }
-    });
+    fs.writeFile(filePath, body, 'utf-8');
   }
 });
